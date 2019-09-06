089bcf7b814bc1b7763bc4919c164a5ed8331970
749300 <749300@elf7it219015.ACDM.DS.SAIT.CA>
2019-09-06 16:58:33 -0600

    New Project Creation Created a new Java Application for HelloWorld.

---
+++ src/hello/word/HelloWord.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.word;
+
+/**
+ *
+ * @author 749300
+ */
+public class HelloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   System.out.print("hello my name is lucky");
+    }
+    
+}
